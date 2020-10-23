fcd87e1263d1992c2ce0fb21ee510c56ef53e98d
jclasley <jon.lasley@gmail.com>
2020-10-23 12:21:52 -0400

    Reverse string complete

--- src/recursion.js
+++ src/recursion.js
@@ -84,7 +84,11 @@ var powerOfTwo = function (n) {
 };
 
 // 9. Write a function that reverses a string.
-var reverse = function(string) {
+var reverse = function (string) {
+  if (string.length === 1) {
+    return string[0];
+  }
+  return string[string.length - 1] + reverse(string.substring(0, string.length - 1));
 };
 
 // 10. Write a function that determines if a string is a palindrome.
